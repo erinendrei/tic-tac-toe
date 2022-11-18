e6bd611e0dfd5e1b0e98d550daba8b5644e7d450
erin <erin.endrei@gmail.com>
2022-11-18 17:04:00 +1000

    Move restart functionality back to gameboard module

--- script.js
+++ script.js
@@ -1,8 +1,70 @@
 const Gameboard = (function () {
     const gameboard = []
+    const gridDivs = document.querySelectorAll(".square")
+
+    const checkArray = (array, currentPlayer) => {
+
+        const winning = [[0, 1, 2], [3, 4, 5],
+        [6, 7, 8], [0, 4, 8],
+        [2, 4, 6], [0, 3, 6], [1, 4, 7],
+        [2, 5, 8]]
+        for (i = 0; i < winning.length; i++) {
+            currentWinning = winning[i]
+            if ((array[currentWinning[0]] == currentPlayer.symbol) && (array[currentWinning[1]] == currentPlayer.symbol) && (array[currentWinning[2]] == currentPlayer.symbol)) {
+                gameIsWon(currentPlayer)
+            }
+        }
+    }
+
+    const addMark = function (e) {
+        gameboard[e.target.dataset.id] = Game.currentPlayer.symbol
+        renderArray(gameboard)
+        checkArray(gameboard, Game.currentPlayer)
+        Game.currentPlayer = Game.togglePlayer(Game.currentPlayer)
+    }
 
 
-    return { gameboard }
+    const addCellListeners = () => {
+        gridDivs.forEach(div => div.addEventListener('click', addMark))
+    }
+
+    addCellListeners()
+
+    const renderArray = (array) => {
+
+        gridDivs.forEach(div => { div.textContent = array[div.dataset.id] })
+
+    }
+
+    const wonDiv = document.getElementById("won")
+
+    const restartGame = () => {
+        newGameButton.classList.add("inactive")
+        wonDiv.classList.add("inactive")
+        for (let i = 0; i < gameboard.length; i++) {
+            gameboard[i] = ""
+        }
+        renderArray(gameboard)
+        addCellListeners()
+        Game.currentPlayer = Game.playerX
+    }
+
+    const newGameButton = btn = document.getElementById("new-game")
+    newGameButton.addEventListener('click', restartGame)
+
+    const showNewGameButton = () => {
+        newGameButton.classList.remove("inactive")
+    }
+    const showWonMessage = (player) => {
+        wonDiv.textContent = `${player.name} won!`
+    }
+    const gameIsWon = (player) => {
+        showWonMessage(player)
+        gridDivs.forEach(div => div.removeEventListener('click', addMark))
+        showNewGameButton()
+    }
+
+    return { gameboard, gridDivs, addMark, renderArray, addCellListeners }
 })()
 
 const playerFactory = (name, symbol) => {
@@ -15,7 +77,9 @@ const Game = (function (gameBoard) {
     const playerX = playerFactory('Player 1', 'X')
     const playerO = playerFactory('Player 2', 'O')
 
-    const togglePlayer = function (player) {
+    let currentPlayer = playerX
+
+    const togglePlayer = (player) => {
         if (player == playerX) {
             player = playerO
         }
@@ -24,45 +88,7 @@ const Game = (function (gameBoard) {
         return player
     }
 
-    let currentPlayer = playerX
-    const gridDivs = document.querySelectorAll(".square")
-
-    const checkArray = (array, currentPlayer) => {
-
-        const winning = [[0, 1, 2], [3, 4, 5],
-        [6, 7, 8], [0, 4, 8],
-        [2, 4, 6], [0, 3, 6],
-        [2, 5, 8]]
-        console.log(currentPlayer.symbol)
-        for (i = 0; i < winning.length; i++) {
-            currentWinning = winning[i]
-            if ((gameBoard[currentWinning[0]] == currentPlayer.symbol) && (gameBoard[currentWinning[1]] == currentPlayer.symbol) && (gameBoard[currentWinning[2]] == currentPlayer.symbol)) {
-                //gameIsWon(currentPlayer)
-                const wonDiv = document.getElementById("won")
-                wonDiv.textContent = `${currentPlayer.name} won!`
-                console.log('Won')
-            }
-        }
-    }
-
-    const addMark = function (e) {
-        gameBoard[e.target.dataset.id] = currentPlayer.symbol
-        e.target.textContent = currentPlayer.symbol
-        checkArray(gameBoard, currentPlayer)
-        currentPlayer = togglePlayer(currentPlayer)
-    }
-
-    gridDivs.forEach(div => div.addEventListener('click', addMark))
-
-
-    const renderArray = (array) => {
-
-        gridDivs.forEach(div => { div.textContent = array[div.dataset.id] })
-
-    }
-
-
-    return { playerFactory, currentPlayer, playerX, playerO }
+    return { togglePlayer, currentPlayer, playerX, playerO }
 })(Gameboard)
 
 
