f90985b7a682c8c7364413aa3143d1ac8ebcaa34
erin <erin.endrei@gmail.com>
2022-11-20 05:47:01 +1000

    Check for tie

--- script.js
+++ script.js
@@ -1,140 +1,150 @@
 const Gameboard = (function () {
-    const gameboard = []
-    const gridDivs = document.querySelectorAll(".square")
+  const gameboard = [];
+  const gridDivs = document.querySelectorAll(".square");
+  let moveCount = 0;
 
-    const checkArray = (array, currentPlayer) => {
-
-        const winning = [[0, 1, 2], [3, 4, 5],
-        [6, 7, 8], [0, 4, 8],
-        [2, 4, 6], [0, 3, 6], [1, 4, 7],
-        [2, 5, 8]]
-        for (i = 0; i < winning.length; i++) {
-            currentWinning = winning[i]
-            if ((array[currentWinning[0]] == currentPlayer.symbol) && (array[currentWinning[1]] == currentPlayer.symbol) && (array[currentWinning[2]] == currentPlayer.symbol)) {
-                gameIsWon(currentPlayer)
-            }
-        }
+  const checkArray = (array, currentPlayer) => {
+    const winning = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+    for (i = 0; i < winning.length; i++) {
+      currentWinning = winning[i];
+      if (
+        array[currentWinning[0]] == currentPlayer.symbol &&
+        array[currentWinning[1]] == currentPlayer.symbol &&
+        array[currentWinning[2]] == currentPlayer.symbol
+      ) {
+        gameOver();
+        showWonMessage(currentPlayer);
+      } else if (moveCount === 9) {
+        gameOver();
+        showTieMessage();
+      }
     }
+  };
 
-    const addMark = function (e) {
-        gameboard[e.target.dataset.id] = Game.currentPlayer.symbol
-        renderArray(gameboard)
-        checkArray(gameboard, Game.currentPlayer)
-        Game.currentPlayer = Game.togglePlayer(Game.currentPlayer)
+  const addMark = function (e) {
+    gameboard[e.target.dataset.id] = Game.currentPlayer.symbol;
+    moveCount++;
+    renderArray(gameboard);
+    checkArray(gameboard, Game.currentPlayer);
+    Game.currentPlayer = Game.togglePlayer(Game.currentPlayer);
+  };
+
+  const addCellListeners = () => {
+    gridDivs.forEach((div) => div.addEventListener("click", addMark));
+  };
+
+  addCellListeners();
+
+  const renderArray = (array) => {
+    gridDivs.forEach((div) => {
+      div.textContent = array[div.dataset.id];
+    });
+  };
+
+  const wonDiv = document.getElementById("won");
+
+  const restartGame = () => {
+    newGameButton.classList.add("inactive");
+    wonDiv.classList.add("inactive");
+    for (let i = 0; i < gameboard.length; i++) {
+      gameboard[i] = "";
     }
+    renderArray(gameboard);
+    addCellListeners();
+    Game.currentPlayer = Game.playerX;
+    moveCount = 0;
+  };
 
+  const newGameButton = (btn = document.getElementById("new-game"));
+  newGameButton.addEventListener("click", restartGame);
 
-    const addCellListeners = () => {
-        gridDivs.forEach(div => div.addEventListener('click', addMark))
-    }
+  const showNewGameButton = () => {
+    newGameButton.classList.remove("inactive");
+  };
+  const showWonMessage = (player) => {
+    wonDiv.textContent = `${player.name} won!`;
+  };
 
-    addCellListeners()
+  const showTieMessage = () => {
+    wonDiv.textContent = `It's a tie.`;
+  };
 
-    const renderArray = (array) => {
+  const gameOver = () => {
+    wonDiv.classList.remove("inactive");
+    removeCellListeners();
+    showNewGameButton();
+  };
 
-        gridDivs.forEach(div => { div.textContent = array[div.dataset.id] })
+  const removeCellListeners = () => {
+    gridDivs.forEach((div) => div.removeEventListener("click", addMark));
+  };
 
-    }
-
-    const wonDiv = document.getElementById("won")
-
-    const restartGame = () => {
-        newGameButton.classList.add("inactive")
-        wonDiv.classList.add("inactive")
-        for (let i = 0; i < gameboard.length; i++) {
-            gameboard[i] = ""
-        }
-        renderArray(gameboard)
-        addCellListeners()
-        Game.currentPlayer = Game.playerX
-    }
-
-    const newGameButton = btn = document.getElementById("new-game")
-    newGameButton.addEventListener('click', restartGame)
-
-    const showNewGameButton = () => {
-        newGameButton.classList.remove("inactive")
-    }
-    const showWonMessage = (player) => {
-        wonDiv.classList.remove('inactive')
-        wonDiv.textContent = `${player.name} won!`
-    }
-    const gameIsWon = (player) => {
-        showWonMessage(player)
-        gridDivs.forEach(div => div.removeEventListener('click', addMark))
-        showNewGameButton()
-    }
-
-    return { gameboard, gridDivs, addMark, renderArray, addCellListeners }
-})()
+  return { gameboard, gridDivs, addMark, renderArray, addCellListeners };
+})();
 
 const playerFactory = (name, symbol, code) => {
-    const sayName = () => { console.log(name) }
+  const sayName = () => {
+    console.log(name);
+  };
 
-    const setName = (newName) => { name = newName }
-    return { name, symbol, code, sayName, setName }
-}
+  const setName = (newName) => {
+    name = newName;
+  };
+  return { name, symbol, code, sayName, setName };
+};
 
 const Game = (function (gameBoard) {
+  const playerX = playerFactory("Player 1", "X", "player_1");
+  const playerO = playerFactory("Player 2", "O", "player_2");
 
-    const playerX = playerFactory('Player 1', 'X', 'player_1')
-    const playerO = playerFactory('Player 2', 'O', 'player_2')
+  let currentPlayer = playerX;
 
-    let currentPlayer = playerX
+  const togglePlayer = (player) => {
+    if (player == playerX) {
+      player = playerO;
+    } else {
+      player = playerX;
+    }
 
-    const togglePlayer = (player) => {
-        if (player == playerX) {
-            player = playerO
+    return player;
+  };
+
+  const addKeyupListeners = (input, player, nameOrSymbol) => {
+    input.addEventListener("keyup", function (event) {
+      event.preventDefault();
+      console.log(player.code);
+      let div = document.getElementById(`${player.code}_${nameOrSymbol}_value`);
+      if (event.code === "Enter") {
+        div.textContent = input.value;
+        div.classList.remove("inactive");
+        input.classList.add("inactive");
+        if (nameOrSymbol == "name") {
+          player.name = input.value;
+        } else if (nameOrSymbol == "symbol") {
+          player.symbol = input.value;
         }
-        else { player = playerX }
+      }
+    });
+  };
+  const addInputListeners = (player) => {
+    let inputName = document.getElementById(`${player.code}_name`);
+    let inputSymbol = document.getElementById(`${player.code}_symbol`);
 
-        return player
-    }
+    addKeyupListeners(inputName, player, "name");
+    addKeyupListeners(inputSymbol, player, "symbol");
+  };
 
-    const setPlayerName = (player, name) => {
-        player.name = name
-    }
+  addInputListeners(playerX);
+  addInputListeners(playerO);
 
-    const setPlayerSymbol = (player, symbol) => {
-        player.symbol = symbol
-    }
-
-    const addKeyupListeners = (input, player, nameOrSymbol) => {
-        input.addEventListener("keyup", function (event) {
-            event.preventDefault();
-            console.log(player.code)
-            let div = document.getElementById(`${player.code}_${nameOrSymbol}_value`)
-            if (event.code === 'Enter') {
-                div.textContent = input.value
-                div.classList.remove('inactive');
-                input.classList.add('inactive')
-                if (nameOrSymbol == 'name') {
-                    setPlayerName(player, input.value)
-                }
-                else if (nameOrSymbol == 'symbol') {
-                    setPlayerSymbol(player, input.value)
-                }
-            }
-        })
-    }
-    const addInputListeners = (player) => {
-        let inputName = document.getElementById(`${player.code}_name`)
-        let inputSymbol = document.getElementById(`${player.code}_symbol`)
-
-        addKeyupListeners(inputName, player, 'name')
-        addKeyupListeners(inputSymbol, player, 'symbol')
-
-
-    }
-
-    addInputListeners(playerX)
-    addInputListeners(playerO)
-
-
-
-
-    return { togglePlayer, currentPlayer, playerX, playerO }
-})(Gameboard)
-
-
-
+  return { togglePlayer, currentPlayer, playerX, playerO };
+})(Gameboard);
